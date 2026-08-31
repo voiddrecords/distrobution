823511e1f58f3357fbd98b3f3d809c4e1f2fb9bd
voiddrecords <114692098+voiddrecords@users.noreply.github.com>
2026-08-31 09:18:13 -0500

    Create supabase.js

---
+++ js/supabase.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = "https://kttpyshyutdmxhcqekxh.supabase.co/rest/v1/";
+const SUPABASE_KEY = "sb_publishable_W-r75b5qVPiikM20aF8NwA_I4h5lhau";
+
+const supabaseClient = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
